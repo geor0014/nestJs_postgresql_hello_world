067311f260a810f32cc9f960f9875713c48d22c0
geor0014 <geor0014@hz.nl>
2023-08-02 09:00:26 +0200

    6 - add validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // this will remove any properties that are not in the DTO
+      forbidNonWhitelisted: true, // this will throw an error if there are properties that are not in the DTO
+    }),
+  ); // this enables the validation pipe for all routes
   await app.listen(3000);
 }
 bootstrap();
--- src/user/createUserDto.ts
+++ src/user/createUserDto.ts
@@ -1,7 +1,13 @@
+import { IsString, IsNumber } from 'class-validator';
+
 // a DTO is a description of how an object should be shaped
 export class createUserDto {
+  @IsString()
   name: string;
+  @IsNumber()
   age: number;
+  @IsString()
   email: string;
+  @IsString()
   password: string;
 }
