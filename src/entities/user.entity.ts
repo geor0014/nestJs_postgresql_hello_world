fe0d3cae4e14d471067fb81c8240ffc92abb0959
geor0014 <geor0014@hz.nl>
2023-08-03 07:03:34 +0200

    8 - add docker-compose file for postgresql and add entities folder and user entity ORM

---
+++ src/entities/user.entity.ts
@@ -0,0 +1,21 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column({
+    unique: true,
+    nullable: false,
+  })
+  name: string;
+  @Column({
+    unique: true,
+    nullable: false,
+  })
+  email: string;
+  @Column({
+    nullable: false,
+  })
+  password: string;
+}
