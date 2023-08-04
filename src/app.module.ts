74c01e4dc47d4d4b1c43fdea7b5718df0fec193e
geor0014 <geor0014@hz.nl>
2023-08-04 08:10:27 +0200

    12 - add auth module and controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
 import config from '../ormconfig';
 
 @Module({
-  imports: [UserModule, TypeOrmModule.forRoot(config)],
+  imports: [UserModule, TypeOrmModule.forRoot(config), AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
